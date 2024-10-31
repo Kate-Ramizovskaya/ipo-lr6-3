3b8b16da6cf163d841b7c9ec88cdbd12848cd981
Kate Ramizovskaya <kr978407@gmail.com>
2024-10-31 09:23:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-kolv=input("Введите кол-во строк: ")
+kolv=input("Введите кол-во строк: ") #запрос кол-ва строк
 
-if kolv.isdigit() and int(kolv)>0:
+if kolv.isdigit() and int(kolv)>0:#проверка на правильный ввод
     kolv=int(kolv)
 else:
     print("Введены неправильные значения.")
 
-str=[]
-for i in range(kolv):
+str=[]#инициализация 
+for i in range(kolv):#ввод строк через цикл for
     strs=input(f"Введите {i+1}: ")
     str.append(strs)
 
-text=' '.join(str)
+text=' '.join(str)#перекидывание списка в строку
 
-words=text.split()
+words=text.split()#разделение на подстроки
 
-words=set(words)
-count=len(words)
+words=set(words)#контейнер не повторяющихся значений
+count=len(words)#подсчет words
 
-print(f"Кол-во различных слов: {count}")
+print(f"Кол-во различных слов: {count}")#итоговый вывод значений 
